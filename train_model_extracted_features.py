ddcb3f7c7e97b1ee439210a9f8305112647f62b5
Abhy Devalapura <101434846+abhydev308@users.noreply.github.com>
2022-05-28 16:10:12 -0700

    Separated Files

--- train_model_extracted_features.py
+++ train_model_extracted_features.py
@@ -1,7 +1,4 @@
 import matplotlib.pyplot as plt
-plt.rcParams["figure.figsize"] = (20, 10)
-plt.rcParams.update({'font.size': 16})
-
 import sklearn
 from sklearn.preprocessing import LabelEncoder, Normalizer, OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
@@ -12,8 +9,8 @@ import numpy as np
 import pickle
 import pandas as pd
 
-# Initialize Multinomial Regression
-clf = sklearn.linear_model.LogisticRegression(class_weight='balanced', multi_class='multinomial', solver='lbfgs', max_iter=10000)
+plt.rcParams["figure.figsize"] = (20, 10)
+plt.rcParams.update({'font.size': 16})
 
 # import data
 x_data = pd.read_pickle('extracted_features_200')
@@ -22,8 +19,6 @@ with open('data_labels_new', 'rb') as f:
     y_data = pickle.load(f)
 
 x_data = x_data.to_numpy()
-
-x_data.shape
 
 # integer encoder
 label_encoder = LabelEncoder()
@@ -36,8 +31,6 @@ onehot_encoder = OneHotEncoder(sparse=False)
 integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
 onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
 
-onehot_encoded
-
 # split data into train-valid-test
 X_train, X_test, y_train, y_test = train_test_split(x_data, onehot_encoded, test_size=0.2, random_state=1)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = 0.25, random_state=1)
@@ -48,93 +41,3 @@ normalized_train_X = scaler.fit_transform(X_train)
 normalized_test_X = scaler.transform(X_test)
 normalized_val_X = scaler.transform(X_val)
 
-# fit model
-clf.fit(normalized_train_X, np.ravel(y_train, order = 'C'))
-
-# calculate model accuracy
-sm_score = clf.score(normalized_test_X, y_test)
-
-sm_score
-
-predictions = clf.predict(normalized_test_X)
-
-precision_score(y_test, predictions, average=None)
-
-recall_score(y_test, predictions, average=None)
-
-fig = plt.figure()
-cm = plot_confusion_matrix(clf, normalized_test_X, y_test)
-plt.matshow(cm)
-fig.savefig('confusion_softmax.png')
-
-from tensorflow import keras
-import tensorflow as tf
-from tensorflow.keras import layers, models, regularizers, metrics
-
-X_train, X_test, y_train, y_test = train_test_split(x_data, onehot_encoded, test_size=0.2, random_state=1)
-scaler = StandardScaler()
-normalized_train_X = scaler.fit_transform(X_train)
-normalized_test_X = scaler.transform(X_test)
-
-def base_model():
-    X_input = layers.Input(452,)
-    X = layers.Dense(128, activation='relu', kernel_regularizer = regularizers.l2(0.01))(X_input)
-    X = layers.Dense(64, activation='relu')(X)
-    X = layers.Dense(32, activation='relu')(X)
-    X = layers.Dense(4, activation='softmax')(X)
-    model = models.Model(inputs = X_input, outputs=X, name='baseline')
-    
-    return model
-
-model = base_model()
-model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-
-history = model.fit(normalized_train_X, y_train, validation_data = (normalized_val_X, y_val), epochs = 15, batch_size=64, shuffle=True)
-
-results = model.evaluate(normalized_test_X, y_test)
-
-# PCA (DOES NOT WORK WELL, need > 50 Components to get good fit)
-
-from sklearn.decomposition import PCA
-
-X_train, X_test, y_train, y_test = train_test_split(x_data, onehot_encoded, test_size=0.2, random_state=1)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
-
-scaler = StandardScaler()
-normalized_train_X = scaler.fit_transform(X_train)
-normalized_test_X = scaler.transform(X_test)
-normalized_val_X = scaler.transform(X_val)
-
-pca = PCA(n_components= 'mle', svd_solver = 'full')
-pca.fit(normalized_train_X)
-print(np.sum(pca.explained_variance_ratio_))
-
-reduced_train_X = pca.transform(normalized_train_X)
-reduced_test_X = pca.transform(normalized_test_X)
-redcued_val_X = pca.transform(normalized_val_X)
-
-clf = sklearn.linear_model.LogisticRegression(class_weight='balanced', multi_class='multinomial', solver='lbfgs', max_iter=100000)
-
-clf.fit(reduced_train_X, y_train)
-
-sm_score = clf.score(reduced_test_X, y_test)
-
-sm_score
-
-def base_model():
-    X_input = layers.Input(445,)
-    X = layers.Dense(128, activation='relu', kernel_regularizer = regularizers.l2(0.01))(X_input)
-    X = layers.Dense(64, activation='relu')(X)
-    X = layers.Dense(32, activation='relu')(X)
-    X = layers.Dense(4, activation='softmax')(X)
-    model = models.Model(inputs = X_input, outputs=X, name='something')
-    
-    return model
-
-model = base_model()
-model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-
-history = model.fit(reduced_train_X, y_train, validation_data = (redcued_val_X, y_val), 
-                    epochs = 20, batch_size=64, shuffle=True)
-
-results = model.evaluate(reduced_test_X, y_test, batch_size=128)
